536c71ee06c58c6ea915879a6214ef97fab2840c
Marcos Vinicius <maarcosvnl@gmail.com>
2020-09-30 00:04:17 -0300

    Add project files.

---
+++ WebSQLSERVER/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSQLSERVER.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
